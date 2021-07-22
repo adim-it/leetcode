baf8c7d680b1a6e1bbd3cc97404fa79ce5ae9ade
rim-dev <36656959+rim-dev@users.noreply.github.com>
2021-07-22 13:29:48 +0900

    Time: 1 ms (100.00%), Space: 36.2 MB (46.41%) - LeetHub

---
+++ reverse-integer/reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+	   long sum = 0;
+	   int tmp = 0;
+	   
+	   while(x != 0) {
+		   sum *= 10;
+		   sum +=  x % 10;
+		   x = x / 10; 
+		   
+		   if(sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+			   return 0;
+		   }
+		   
+	   }
+        
+        return (int)sum;
+    }
+}
